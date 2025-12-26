657c2c147bc0505d9bc36c9268c46f9cd5d27492
Harshit Agrawal <harshit.agrawal@wehealapp.in>
2025-12-26 13:03:35 +0530

    adding custom function into themes

--- JPC_library/src/main/java/com/example/jpc_library/ui/theme/Theme.kt
+++ JPC_library/src/main/java/com/example/jpc_library/ui/theme/Theme.kt
@@ -2,13 +2,21 @@ package com.example.jpc_library.ui.theme
 
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
 import androidx.compose.ui.platform.LocalContext
+
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
@@ -55,3 +63,55 @@ fun Global_Theme_JPCTheme(
       content = content
     )
 }
+
+
+
+
+@Composable
+fun Custom_Theme_JPC(
+    themeIndex: Int,
+    content: @Composable () -> Unit
+) {
+    val colors = when (themeIndex) {
+        2 -> lightColorScheme(
+            primary = Color(0xFFEA7068),
+            background = Color(0xFFA47868),
+            surface = Color(0xFF7C380B)
+        )
+        3 -> lightColorScheme(
+            primary = Color(0xFF4CAF50),
+            background = Color(0xFF095548),
+            surface = Color(0xFF27670C)
+        )
+        else -> lightColorScheme(
+            surface = Color(0xFF260460)
+        ) // default theme
+    }
+
+    MaterialTheme(colorScheme = colors, content = content)
+}
+
+
+@Composable
+fun StyledButton(
+    text: String,
+    colors: ButtonColors,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Button(onClick = onClick, colors = colors, modifier = modifier) {
+        Text(text)
+    }
+}
+
+@Composable
+fun resolveButtonColors(isSelected: Boolean): ButtonColors =
+    ButtonDefaults.buttonColors(
+        containerColor =
+            if (isSelected) MaterialTheme.colorScheme.surface
+            else MaterialTheme.colorScheme.primary,
+        contentColor = Color.White
+
+        //if (isSelected) MaterialTheme.colorScheme.onPrimary
+        //else MaterialTheme.colorScheme.onSurface
+    )
--- app/src/main/java/com/example/global_theme_jpc/MainActivity.kt
+++ app/src/main/java/com/example/global_theme_jpc/MainActivity.kt
@@ -2,55 +2,56 @@
 package com.example.global_theme_jpc.ui
 
 import android.os.Bundle
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.lightColorScheme
-import kotlinx.coroutines.flow.map
+//import androidx.compose.runtime.getValue
+//import androidx.compose.runtime.setValue
+//import androidx.compose.runtime.Composable
+//import androidx.compose.runtime.mutableStateOf
+//import androidx.compose.runtime.rememberCoroutineScope
 
+import androidx.compose.runtime.*
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.Text
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.TopAppBarDefaults
+
+
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.platform.LocalContext
+
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.ui.unit.dp
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.animation.animateColorAsState
-
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.*
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonColors
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.animation.animateColorAsState
 
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-
-import android.content.Context
-import androidx.compose.ui.platform.LocalContext
 import androidx.datastore.preferences.preferencesDataStore
 import androidx.datastore.preferences.core.intPreferencesKey
 import androidx.datastore.preferences.core.edit
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Alignment
 
 
-import kotlinx.coroutines.launch
-import androidx.compose.ui.platform.LocalContext
-import com.example.jpc_library.ui.theme.Global_Theme_JPCTheme
+
+import android.content.Context
+
+import com.example.global_theme_jpc.ui.*
+import com.example.global_theme_jpc.ui.theme.*
+
 import kotlinx.coroutines.flow.map
-import java.time.temporal.TemporalAdjusters.next
+import kotlinx.coroutines.launch
 
+import com.example.jpc_library.ui.theme.*
+import com.example.jpc_library.ui.theme.Custom
 
 val Context.dataStore by preferencesDataStore(name = "settings")
 
@@ -75,7 +76,7 @@ class MainActivity : ComponentActivity() {
                 .map { it[PrefKeys.THEME_INDEX] ?: 1 }
                 .collectAsState(initial = 1)
             Global_Theme_JPCTheme()
-            Global_Theme_JPC(themeIndex) {
+            Custom_Theme_JPC(themeIndex) {
 
                 Scaffold(
                     topBar = {
@@ -120,63 +121,8 @@ class MainActivity : ComponentActivity() {
 
 
 
-@Composable
-fun Global_Theme_JPC(
-    themeIndex: Int,
-    content: @Composable () -> Unit
-) {
-    val colors = when (themeIndex) {
-        2 -> lightColorScheme(
-            primary = Color(0xFFEA7068),
-            background = Color(0xFFA47868),
-            surface = Color(0xFF7C380B)
-        )
-        3 -> lightColorScheme(
-            primary = Color(0xFF4CAF50),
-            background = Color(0xFF095548),
-            surface = Color(0xFF27670C)
-        )
-        else -> lightColorScheme(
-            surface = Color(0xFF260460)
-        ) // default theme
-    }
-
-    MaterialTheme(colorScheme = colors, content = content)
-}
-
-data class ButtonStyle(
-    val containerColor: Color,
-    val contentColor: Color,
-    //val textSize: TextUnit,
-    //val shape: Shape,
-    //val elevation: Dp
-)
 
 
-
-@Composable
-fun StyledButton(
-    text: String,
-    colors: ButtonColors,
-    onClick: () -> Unit,
-    modifier: Modifier = Modifier
-) {
-    Button(onClick = onClick, colors = colors, modifier = modifier) {
-        Text(text)
-    }
-}
-
-@Composable
-fun resolveButtonColors(isSelected: Boolean): ButtonColors =
-    ButtonDefaults.buttonColors(
-            containerColor =
-            if (isSelected) MaterialTheme.colorScheme.surface
-            else MaterialTheme.colorScheme.primary,
-            contentColor = Color.White
-
-            //if (isSelected) MaterialTheme.colorScheme.onPrimary
-            //else MaterialTheme.colorScheme.onSurface
-    )
 
 @Composable
 fun MainScreen(
@@ -209,17 +155,17 @@ fun MainScreen(
         StyledButton(text = "My Button1",colors = resolveButtonColors(isBtn1Selected), onClick = { isBtn1Selected = !isBtn1Selected })
         StyledButton(text = "My Button2",colors = resolveButtonColors(isBtn2Selected), onClick = { isBtn2Selected = !isBtn2Selected })
 
-            Button(modifier = defaultBtn,onClick = onToggleTheme) { Text("Switch Themes") }
+        Button(modifier = defaultBtn,onClick = onToggleTheme) { Text("Switch Themes") }
 
 
 
 
-            // Theme toggle button (normal button)
+        // Theme toggle button (normal button)
 
-            //  Box is must to ensure independent placement of buttons
+        //  Box is must to ensure independent placement of buttons
 
 
-            // Material buttons (ALL derive color from MaterialTheme)
+        // Material buttons (ALL derive color from MaterialTheme)
 
 
 
